9514af7ddc63d69a0d12669fb79f6a456f148bc5
minh_dq <dquangminh98@gmail.com>
2021-03-21 22:14:22 +0700

    feature#35/ListConnections

---
+++ src/components/molecules/ConnectionListItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <a class="connection-container" href="#">
+      <div class="connection-info">
+        <h4>{{ connection.request_type}}</h4>
+        <div class="connection-description">{{ connection.description }}</div>
+      </div>
+      <div class="connection-meta">
+          <h4>Created at:</h4>
+          <p>{{ createdAt }}</p>
+      </div>
+  </a>
+</template>
+
+<script>
+export default {
+    name:"connection-list-item",
+    props: {
+        connection: Object
+    },
+    computed: {
+        createdAt() {
+            return Date(this.connection.created_at)
+        }
+    }
+}
+</script>
+
+<style scoped>
+.connection-container {
+    display: flex;
+    height: 116px;
+    border: 1px solid var(--boxshadowcolour2);
+    margin-bottom: 10px;
+    padding: 20px;
+    transition: all 0.25s;
+    text-decoration: none;
+    color: var(--textgray);
+}
+
+.connection-info {
+    width: calc(100% - 200px)
+}
+
+.connection-description {
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    height: 48px;
+}
+
+.connection-meta {
+    width: 200px;
+}
+
+
+.connection-container:hover {
+    box-shadow: 0 0 2.5px 2px var(--boxshadowcolour2);
+    cursor: pointer;
+}
+</style>
---
+++ src/components/molecules/Pagination.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <page-number 
+      v-for="(page, index) in pages" 
+      :key="index" 
+      :number=page 
+      :isActive="page == activePage"
+      @click="$emit('setPage', page)"></page-number>
+  </div>
+</template>
+
+<script>
+import PageNumber from '../atoms/PageNumber.vue'
+
+export default {
+    name: 'pagination',
+    components: { PageNumber},
+    props: {
+        totalItems: Number,
+        activePage: Number,
+        itemsPerPage: Number 
+    },
+    computed: {
+        pages() {
+            let totalPages = Math.floor(this.totalItems / this.itemsPerPage) + (this.totalItems % this.itemsPerPage == 0 ? 0 : 1)
+            let pageList = []
+            for (let i=1; i<= totalPages; i ++) {
+                pageList.push(i)
+            }
+            return pageList
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
