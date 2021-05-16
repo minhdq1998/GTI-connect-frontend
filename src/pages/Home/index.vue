afe009b0ae8868576f997951ef93271e27b9c1bf
Giang Le <giangle0411@gmail.com>
2021-05-17 02:44:40 +1000

    Landing page

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="">
-    <h1>Underconstruction</h1>
+  <div class="landing-container">
+    <welcome />
+    <how-we-work />
+    <payment-info />
   </div>
 </template>
 
 <script>
+import Welcome from './components/Welcome.vue'
+import HowWeWork from './components/HowWeWork.vue'
+import PaymentInfo from './components/PaymentInfo.vue'
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  components: { Welcome, HowWeWork, PaymentInfo },
   name: 'Home',
-  components: {
-  }
+ 
 }
 </script>
+
+
+<style lang="scss" scoped>
+
+</style>
